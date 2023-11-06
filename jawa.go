1d1ef7f77899d8a20f717644e9cb85477fb3f6f7
Naufal Anas <92364025+naufalanasea@users.noreply.github.com>
2023-11-06 17:34:51 +0700

    Create jawa.go

---
+++ jawa.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+  // Pilihan pulau
+  var pulau string
+  fmt.Println("Pilih pulau:")
+  fmt.Println("1. Sumatera")
+  fmt.Println("2. Kalimantan")
+  fmt.Println("3. Papua")
+  fmt.Println("4. Bali")
+  fmt.Println("5. Sulawesi")
+  fmt.Println("6. Nusa Tenggara")
+  fmt.Scan(&pulau)
+
+  // Kondisi
+  var jawa bool = pulau == "jawa"
+
+  // Output
+  if jawa {
+    fmt.Println("Jawa adalah kunci")
+  } else {
+    fmt.Println("Pulau yang dipilih adalah", pulau)
+  }
+}
