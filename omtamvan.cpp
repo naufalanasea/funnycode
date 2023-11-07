fe25c5e8cabfade14d2bef4d7880a4d7e6dcf0a7
Naufal Anas <92364025+naufalanasea@users.noreply.github.com>
2023-11-07 20:46:32 +0700

    Create omtamvan.cpp
    omtamvan is good mate

---
+++ omtamvan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string nama;
+  cout << "Masukkan nama: ";
+  cin >> nama;
+
+  if (nama == "omtamvan") {
+    cout << "omtamvan is good" << endl;
+  } else {
+    cout << "is not good" << endl;
+  }
+
+  return 0;
+}
