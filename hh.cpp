68bd786fc2530f8cf443c7a16f9bf5bd65c0ac71
Naufal Anas <92364025+naufalanasea@users.noreply.github.com>
2024-02-08 20:49:30 +0700

    Create hh.cpp

---
+++ hh.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+  // Array untuk menyimpan karakter suara tawa
+  char laugh[] = "hahahahahahahaha";
+
+  // Loop untuk mengulang suara tawa
+  for (int i = 0; i < 10; i++) {
+    // Mencetak karakter suara tawa
+    std::cout << laugh;
+  }
+
+  // Menambahkan baris baru
+  std::cout << std::endl
+
+  return 0;
+}
